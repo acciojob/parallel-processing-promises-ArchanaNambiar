e75969e0e7f87dacddd88e2838fa85dbb0de5bed
ArchanaNambiar <64783368+ArchanaNambiar@users.noreply.github.com>
2023-07-31 21:26:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,39 +7,33 @@ const images = [
   { url: "https://picsum.photos/id/238/200/300" },
   { url: "https://picsum.photos/id/239/200/300" },
 ];
-// Function to download an image
-    function downloadImage(image) {
-      return new Promise((resolve, reject) => {
-        const imgElement = new Image();
-        imgElement.onload = () => resolve(imgElement);
-        imgElement.onerror = () => reject(new Error(`Failed to load image's URL: ${image.url}`));
-        imgElement.src = image.url;
-        imgElement.alt = image.alt;
-      });
-    }
+// Function to download an image using Fetch API
+function downloadImage(image) {
+  return new Promise((resolve, reject) => {
+    const img = new Image();
+    img.onload = () => resolve(img);
+    img.onerror = () => reject(new Error(`Failed to load image's URL: ${image.url}`));
+    img.src = image.url;
+  });
+}
 
-    // Function to display images on the webpage
-    function displayImages(images) {
-      const outputDiv = document.getElementById("output");
-      for (const image of images) {
-        outputDiv.appendChild(image);
-      }
-    }
+// Function to display the images on the webpage
+function displayImages(imagesArray) {
+  output.innerHTML = "";
+  imagesArray.forEach(image => {
+    output.appendChild(image);
+  });
+}
 
-    // Function to handle image download
-    async function downloadAndDisplayImages() {
-      try {
-        // Download all images in parallel using Promise.all
-        const imagePromises = imageUrls.map(downloadImage);
-        const images = await Promise.all(imagePromises);
+// Function to download and display images on button click
+btn.addEventListener("click", () => {
+  const imagePromises = images.map(image => downloadImage(image));
 
-        // Display the downloaded images on the webpage
-        displayImages(images);
-      } catch (error) {
-        console.error("An error occurred while downloading images:", error);
-      }
-    }
-
-    // Attach click event to the download button
-    const downloadButton = document.getElementById("download-images-button");
-    downloadButton.addEventListener("click", downloadAndDisplayImages);
+  Promise.all(imagePromises)
+    .then(imagesArray => {
+      displayImages(imagesArray);
+    })
+    .catch(error => {
+      console.error("An error occurred:", error);
+    });
+});
