eaa96df3fdb62aa84f9cf04afe44c82ff46ce37b
ArchanaNambiar <64783368+ArchanaNambiar@users.noreply.github.com>
2023-08-19 12:52:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,39 +1,31 @@
 //your JS code here. If required.
-const output = document.getElementById("output");
-const btn = document.getElementById("download-images-button");
+const imageUrls = [
+            'url_to_image_1',
+            'url_to_image_2',
+            // Add more image URLs as needed
+        ];
 
-const images = [
-  { url: "https://picsum.photos/id/237/200/300" },
-  { url: "https://picsum.photos/id/238/200/300" },
-  { url: "https://picsum.photos/id/239/200/300" },
-];
-// Function to download an image using Fetch API
-function downloadImage(image) {
-  return new Promise((resolve, reject) => {
-    const img = new Image();
-    img.onload = () => resolve(img);
-    img.onerror = () => reject(new Error(`Failed to load image's URL: ${image.url}`));
-    img.src = image.url;
-  });
-}
+        const outputDiv = document.getElementById('output');
+        const downloadButton = document.getElementById('download-images-button');
 
-// Function to display the images on the webpage
-function displayImages(imagesArray) {
-  output.innerHTML = "";
-  imagesArray.forEach(image => {
-    output.appendChild(image);
-  });
-}
+        function loadImage(url) {
+            return new Promise((resolve, reject) => {
+                const img = new Image();
+                img.src = url;
+                img.addEventListener('load', () => resolve(img));
+                img.addEventListener('error', () => reject(new Error(`Failed to load image's URL: ${url}`)));
+            });
+        }
 
-// Function to download and display images on button click
-btn.addEventListener("click", () => {
-  const imagePromises = images.map(image => downloadImage(image));
+        downloadButton.addEventListener('click', async () => {
+            outputDiv.innerHTML = ''; // Clear existing content
 
-  Promise.all(imagePromises)
-    .then(imagesArray => {
-      displayImages(imagesArray);
-    })
-    .catch(error => {
-      console.error("An error occurred:", error);
-    });
-});
+            for (const imageUrl of imageUrls) {
+                try {
+                    const imgElement = await loadImage(imageUrl);
+                    outputDiv.appendChild(imgElement);
+                } catch (error) {
+                    console.error(error.message);
+                }
+            }
+        });
